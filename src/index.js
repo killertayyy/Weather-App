4180018622ad350643307811a7e89df2bc0f5e0e
killertayyy <taylorpierce22@gmail.com>
2022-01-22 15:11:55 -0800

    Displayed forecast data using API
    Shortened forecast to 6 days using an index, added days of the week by means of a function and timestamp tool, and rounded the min and max temperature values.

--- src/index.js
+++ src/index.js
@@ -40,33 +40,42 @@ function formatDate(date) {
   return `${currentDay}, ${currentMonth} ${currentDate} </br> ${hour}:${minute}`;
 }
 
+function formatDay(timestamp){
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+return days [day]; 
+}
+
 function displayForecast (response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
-   let days = ["Thu", "Fri", "Sat", "Sun"];
-
   let forecastHTML = `<div class = "row">`;
-  days.forEach(function (day) {
-  forecastHTML = 
+  forecast.forEach(function (forecastDay,index) {
+    if (index < 6) {
+        forecastHTML = 
   forecastHTML + 
 `
             <div class="col-2">
-              <div class="weather-forecast-date">${day}</div>
-                <img id ="icon"
-              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" 
-              alt ="cloudy" 
+              <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+              ${index}
+                <img
+              src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
+              alt ="image" 
               width="42"> </img>
         <div class="weather-forecast-temperatures">
           <span class="weather-forecast-temperature-max">
-            18 °
+            ${Math.round(forecastDay.temp.max)}°
           </span>
           <span class="weather-forecast-temperature-min">
-            12 °
+            ${Math.round(forecastDay.temp.min)}°
           </span>
         </div>
         </div>
         `;
+    }
   });
         forecastHTML = forecastHTML + `</div>`;
         forecastElement.innerHTML = forecastHTML;
